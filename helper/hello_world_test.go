0ae9284720e1ea26a041ba280f790cc733ceec7a
Snow-00 <wariorwilly43@gmail.com>
2023-05-20 14:46:54 +0000

    table test 1

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -19,6 +19,21 @@ assert menggunakan Fail
 require menggunakan FailNow
 
 untuk mengskip unit test bs pake skip test (biasanya ketika di os tertentu)
+
+test main buat menjalankan code sebelum dan atau sesudah testing misal, 
+connect disconnect database
+test main cmn jalan di 1 package aj, kalo ada bny package hrs kasi 1"
+untuk pake test main harus => TestMain
+run test main tinggal jalanin go test kek biasa (milih jalanin func apa)
+
+sub test = func unit test di dlm func unit test
+sub test bs menggunakan func Run()
+go test -v -run TestSubTest/...  => jalanin salah 1 sub test aj
+go test -v -run /NamaSubTest => jalanin smua test dg NamaSubTest itu, yg ga ada sub test lgsg jl
+
+table test = konsep di mana nyediain data berupa,
+slice isi param dan ekspektasi dr unit test, lalu slice diiterasi pake sub test
+
 */
 
 package helper
@@ -30,6 +45,28 @@ import (
   "github.com/stretchr/testify/require"
   "runtime"
 )
+
+func TestSubTest(t *testing.T) {
+  t.Run("will", func(t *testing.T) {
+    result := HelloWorld("will")
+    require.Equal(t, "Hello will", result, "Result must be 'Hello eko'")
+  })
+
+  t.Run("kurniawan", func(t *testing.T) {
+    result := HelloWorld("Kurniawan")
+    require.Equal(t, "Hi Kurniawan", result, "Result must be 'Hello eko'")
+  })
+}
+
+func TestMain(m *testing.M) {
+  // before
+  fmt.Println("before unit test")
+
+  m.Run()
+
+  // after
+  fmt.Println("after unit test")
+}
 
 func TestSkip(t *testing.T) {
   if runtime.GOOS == "linux" {
