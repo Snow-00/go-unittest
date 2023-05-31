1aef51ed0122af06d53b3731ead20c08c1888023
Snow-00 <wariorwilly43@gmail.com>
2023-05-31 16:21:43 +0000

    mock test 4

--- service/category_service_test.go
+++ service/category_service_test.go
@@ -10,12 +10,28 @@ import (
 var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
 var categoryService = CategoryService{Repository: categoryRepository}
 
-func TestCategoryService_Get(t *testing.T) {
+// testing dg harapan hasil nya nil
+func TestCategoryService_GetNotFound(t *testing.T) {
 	// program mock
-	// ketika func FindById (func di repository) dipanggil dg param 1 maka return datanya nil
+	// ketika func FindById (func di repository) dipanggil oleh mock dg param 1 maka return datanya nil
 	categoryRepository.Mock.On("FindById", "1").Return(nil)
 
 	category, err := categoryService.Get("1")
 	assert.Nil(t, category)
 	assert.NotNil(t, err)
+}
+
+// testing dg harapan hasilnya ada / datanya sukses muncul
+func TestCategoryService_GetSuccess(t *testing.T) {
+  category := entity.Category{
+    Id:   "1",
+    Name: "Laptop",
+  }
+  categoryRepository.Mock.On("FindById", "2").Return(category)
+
+  result, err := categoryService.Get("2")
+  assert.Nil(t, err)
+  assert.NotNil(t, result)
+  assert.Equal(t, category.Id, result.Id)
+  assert.Equal(t, category.Name, result.Name)
 }
